3f21910eb367a0f75d7d73fb8abda0faaf16595b
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-09 11:09:19 -0700

    edit

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -2,10 +2,12 @@ export function manageFriends(state, action) {
   // your code here
   switch(action.type){
     case "friends/add":
-      return {friends : [...state.friend, action.payload] }
+      return {friends: [...state.friend, action.payload],
+       };
     case "friends/remove":
       return {
-        friends: state.friends.filter((friend)=> friend.id != action.payload) };
+        friends: state.friends.filter((friend)=> friend.id != action.payload),
+       };
       default:
         return state;
   }
